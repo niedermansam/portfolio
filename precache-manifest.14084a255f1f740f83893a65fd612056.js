595bad3c1f75ec1685183cf9ce775c3abf3bc0c7
niedermansam <niederman.sam@gmail.com>
2019-08-09 14:41:22 -0600

    Updates

---
+++ precache-manifest.14084a255f1f740f83893a65fd612056.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8d9f76598b95f22d9461d67e0c3072",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8b43f2e54324cced43eb",
+    "url": "/portfolio/static/css/main.1ee78f0d.chunk.css"
+  },
+  {
+    "revision": "75b26261069ebffcbcb9",
+    "url": "/portfolio/static/js/2.7a0a9cd9.chunk.js"
+  },
+  {
+    "revision": "8b43f2e54324cced43eb",
+    "url": "/portfolio/static/js/main.c833d6d5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
