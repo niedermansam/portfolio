1e1925c1fba06faea9399820714d20209824382b
niedermansam <niederman.sam@gmail.com>
2019-08-20 14:33:11 -0600

    Updates

---
+++ precache-manifest.6e20e497ab4c11009af28853f1193cb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3d9d7dadac94820918aae319858120",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "20fc9de8175383a4ce1d",
+    "url": "/portfolio/static/css/main.1442bce5.chunk.css"
+  },
+  {
+    "revision": "524e98fcd7442296b3a4",
+    "url": "/portfolio/static/js/2.261595cc.chunk.js"
+  },
+  {
+    "revision": "20fc9de8175383a4ce1d",
+    "url": "/portfolio/static/js/main.d081777c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f1de9dac303baa53d5663cefd23137df",
+    "url": "/portfolio/static/media/census-searcher-tool.f1de9dac.PNG"
+  },
+  {
+    "revision": "f74737a04c9059d1e279ef4843e9bdfd",
+    "url": "/portfolio/static/media/ski-resort-explorer.f74737a0.PNG"
+  },
+  {
+    "revision": "fd76018e472820cbb5f473dc76f2f167",
+    "url": "/portfolio/static/media/trump-twitter-map.fd76018e.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0dbb338ba89c6ffc2aab3abcc2e77c0c.js"
+  "/portfolio/precache-manifest.6e20e497ab4c11009af28853f1193cb2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
